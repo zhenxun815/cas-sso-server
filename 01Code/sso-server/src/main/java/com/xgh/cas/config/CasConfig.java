44f29daee5a8671bf4ea3cfc77478d24f5a2728b
bixuejun <bxjgood@163.com>
2021-01-26 10:38:08 +0800

    1、修改访问CAS-Server访问地址

--- 01Code/sso-server/src/main/java/com/xgh/cas/config/CasConfig.java
+++ 01Code/sso-server/src/main/java/com/xgh/cas/config/CasConfig.java
@@ -5,7 +5,7 @@ public class CasConfig {
     /**
      * CAS登录地址的token
      */
-    public static String GET_TOKEN_URL = "https://cas.server.com:8443/cas/v1/tickets";
+    public static String GET_TOKEN_URL = "https://192.168.2.65:8443/cas/v1/tickets";
 
     /**
      * 设置Cookie的有效时长（1小时）
