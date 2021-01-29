e6673b8fe412b513a6a182df8be1be418a18cff8
bixuejun <bxjgood@163.com>
2021-01-29 08:29:53 +0800

    1、配置域名;

--- 01Code/sso-server/src/main/java/com/xgh/cas/config/CasConfig.java
+++ 01Code/sso-server/src/main/java/com/xgh/cas/config/CasConfig.java
@@ -5,7 +5,9 @@ public class CasConfig {
     /**
      * CAS登录地址的token
      */
-    public static String GET_TOKEN_URL = "https://192.168.2.65:8443/cas/v1/tickets";
+    public static String GET_TOKEN_URL = "https://xgohe.f3322.net:8443/cas/v1/tickets";
+    //public static String GET_TOKEN_URL = "https://192.168.3.185:8443/cas/v1/tickets";
+    //public static String GET_TOKEN_URL = "https://cas.server.com:8443/cas/v1/tickets";
 
     /**
      * 设置Cookie的有效时长（1小时）
